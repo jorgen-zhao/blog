48011b09b38ac5ebe29ca9dae6b614e9d244abbc
jorgen <1129810754@qq.com>
2023-02-07 07:27:43 +0800

    fix:deploy update deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,9 @@ npm run build
 # 进入生成的文件夹
 cd $dist_path
 
+git config --global user.email "1129810754@qq.com"
+git config --global user.name "jorgen"
+
 git init
 git add -A
 git commit -m "deploy, $commit_info"
