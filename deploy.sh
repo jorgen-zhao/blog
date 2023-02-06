9930ef5489940c07e24c6bb23e843dd8bbd19697
jorgen <1129810754@qq.com>
2023-02-06 22:39:46 +0800

    feat: github action

--- deploy.sh
+++ deploy.sh
@@ -10,34 +10,21 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'b.Jorgen.com' > CNAME
+echo 'jorgen.website' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:Jorgen/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:jorgen-zhao/blog.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://Jorgen:${GITHUB_TOKEN}@github.com/Jorgen/vuepress-theme-vdoing.git
-  git config --global user.name "Jorgen"
-  git config --global user.email "894072666@qq.com"
+  githubUrl=https://github.com/jorgen-zhao/blog.git
+  git config --global user.name "jorgen-zhao"
+  git config --global user.email "1129810754@qq.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
-
-# deploy to coding pages
-# echo 'www.Jorgen.com\nJorgen.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
 
 cd -
 rm -rf docs/.vuepress/dist
