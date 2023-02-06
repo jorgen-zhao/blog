1dd3025d4ff475d60c71613f37844591a1c6a5cb
jorgen <1129810754@qq.com>
2023-02-06 22:45:01 +0800

    fix:deloy update sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ if [ -z "$GITHUB_TOKEN" ]; then
   githubUrl=git@github.com:jorgen-zhao/blog.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://github.com/jorgen-zhao/blog.git
+  githubUrl=https://jorgen-zhao:${GITHUB_TOKEN}@github.com/jorgen-zhao/blog.git
   git config --global user.name "jorgen-zhao"
   git config --global user.email "1129810754@qq.com"
 fi
