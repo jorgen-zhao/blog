dbf0a154b39ac8ef3bf5fce672136f7398ab5780
jorgen <1129810754@qq.com>
2023-02-07 22:15:29 +0800

    feat:config update config

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
-git config --global user.email "1129810754@qq.com"
-git config --global user.name "jorgen-zhao"
-
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd $dist_path
-
+# deploy to github
+echo 'jorgen.website' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:jorgen-zhao/blog.git
+else
+  msg='来自github action的自动部署!'
+  githubUrl=https://jorgen-zhao:${GITHUB_TOKEN}@github.com/jorgen-zhao/blog.git
+  git config --global user.name "jorgen-zhao"
+  git config --global user.email "1129810754@qq.com"
+fi
 git init
 git add -A
-git commit -m "github action auto deploy"
-git push -f git@github.com:jorgen-zhao/blog.git master:gh-pages
-
+git commit -m "${msg}"
+git push -f $githubUrl HEAD:gh-pages # 推送到github
 
 cd -
-rm -rf $dist_path
+rm -rf docs/.vuepress/dist
