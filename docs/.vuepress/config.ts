378f79d67b7f1b835f1efc2129a16237c5d5811f
jorgen <1129810754@qq.com>
2023-02-08 06:54:41 +0800

    feat:content update content

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -19,7 +19,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       description: '技术博客,学习与总结。Java,vue,Springboot,MySQL,Docker,Kafka,ES,Node,git,github等技术文章。',
     }
   },
-  base: '/blog/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
   // 主题配置
   themeConfig: {
