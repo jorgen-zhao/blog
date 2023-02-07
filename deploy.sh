a948221a9fc376f7e59982c11320fc9519290c32
jorgen <1129810754@qq.com>
2023-02-08 07:03:24 +0800

    fix:ci update token

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ cd docs/.vuepress/dist # 进入生成的文件夹
 
 # deploy to github
 echo 'jorgen.website' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
+if [ -z "$ACCESS_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:jorgen-zhao/blog.git
 else
   msg='来自github action的自动部署!'
-  githubUrl=https://jorgen-zhao:${GITHUB_TOKEN}@github.com/jorgen-zhao/blog.git
+  githubUrl=https://jorgen-zhao:${ACCESS_TOKEN}@github.com/jorgen-zhao/blog.git
   git config --global user.name "jorgen-zhao"
   git config --global user.email "1129810754@qq.com"
 fi
