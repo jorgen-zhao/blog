6ada1cc12e5e18b67a34f18dea728607ed4c5e19
jorgen <1129810754@qq.com>
2023-02-07 22:04:36 +0800

    feat:config update config

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,7 @@ set -e
 git config --global user.email "1129810754@qq.com"
 git config --global user.name "jorgen-zhao"
 
-# push_addr=`git remote get-url --push origin` # git提交地址
-push_addr=`git@github.com:jorgen-zhao/blog.git` # git提交地址
-commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
 
 # 生成静态文件
 npm run build
@@ -20,8 +16,8 @@ cd $dist_path
 
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git commit -m "github action auto deploy"
+git push -f git@github.com:jorgen-zhao/blog.git master:gh-pages
 
 cd -
 rm -rf $dist_path
